dd00ad2aac7d5af555c976d459dbd1693ebc45ab
Mogul <mogultest1@hotmail.com>
2012-09-02 20:07:08 -0700

    shit

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace MvcApplication1.Controllers
 {
-    public class HomeController : Controller
+   ///hkj public class HomeController : Controller
     {
         public ActionResult Index()
         {
